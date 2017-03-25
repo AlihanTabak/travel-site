33f486f8116af784e923406b6727347095b0154a
mertcancam <mertcancam1@gmail.com>
2017-03-25 14:36:39 +0300

    new layout is made when get in touch or get started today is clicked

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -11,7 +11,6 @@ class StickyHeader {
     this.headerLinks = $(".primary-nav a");
     this.myLinks = $(".my-links");
     this.createPageSectionWaypoints();
-    this.resetPageSectionWaypoints();
     this.addSmoothScrolling();
     
   }
